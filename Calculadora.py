2e43743a7bd930aedf4f9a186d23486e6a26b6c8
L1nk01 <ecampospaulino@gmail.com>
2024-04-17 07:32:45 -0400

    first commit

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+class Calculadora:
+    def suma(self, a, b):
+        resultado = int(a) + int(b)
+        return resultado
+
+    def resta(self, a, b):
+        resultado = int(a) - int(b)
+        return resultado
+
+    def multiplicacion(self, a, b):
+        resultado = int(a) * int(b)
+        return resultado
+
+    def division(self, a, b):
+        resultado = int(a) / int(b)
+        return resultado
